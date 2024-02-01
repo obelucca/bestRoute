6c4297f19699de3dc2f0fd25a5601abbedd515e1
Obelucca <jobs.cleberr@gmail.com>
2024-02-01 09:22:02 -0300

    Server Flask e result PHP

--- DAO/AddressDAO.php
+++ DAO/AddressDAO.php
@@ -1,7 +1,20 @@
 <?php 
 
-require_once __DIR__ . '../vendor/autoload.php';
+$pythonServerURL = "http://127.0.0.1:5000/geocode";
 
-use ipinfo\ipinfo\IPinfo;
+$address = "Av. Cristiano Machado, 11833 - Vila Cloris, Belo Horizonte - MG, 31744-007";
 
+$pythonResponse =  file_get_contents("$pythonServerURL?address=".urlencode($address));
+
+$dadosLocalizacao = json_decode($pythonResponse, true);
+
+if($dadosLocalizacao && isset($dadosLocalizacao['latitude'], $dadosLocalizacao['longitude'])){
+
+    $latitude =  $dadosLocalizacao['latitude'];
+    $longitude = $dadosLocalizacao['longitude'];
+
+    echo "Latitude: $latitude, Longitude $longitude";
+} else {
+    echo "Não foi possivel obter informações da localização do servidor Python";
+}
 
