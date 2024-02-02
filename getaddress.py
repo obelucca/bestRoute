0a6eda8f6c0dd4da4d17b795c90f1d651b16a403
Obelucca <jobs.cleberr@gmail.com>
2024-02-02 09:14:22 -0300

    Getaddress

--- getaddress.py
+++ getaddress.py
@@ -1,6 +1,21 @@
+from flask import Flask, request, jsonify
 from geopy.geocoders import Nominatim
 
+app = Flask(__name__)
 geolocator = Nominatim(user_agent="BestRoute")
-location = geolocator.geocode("Av. Cristiano Machado, 11833 - Vila Cloris, Belo Horizonte - MG, 31744-007")
 
-print(location.point)
+@app.route('/geocode', methods=['GET'])
+
+def geocode():
+    address = request.args.get('address', '')
+    location = geolocator.geocode(address)
+
+    if location:
+        latitude = location.latitude
+        longitude = location.longitude
+        return jsonify({'latitude': latitude, 'longitude': longitude})
+    else: 
+        return jsonify({'error': 'Não foi possível obter informações dessa  localização.'})
+    
+if __name__ == '__main__':
+    app.run(debug=True)
